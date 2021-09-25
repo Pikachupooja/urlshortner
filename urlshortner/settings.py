9d3ffd5cd3ceaa9d4f1c36113f9798201a450a19
unknown <poojachaturvedi1416@gmail.com>
2021-09-25 15:28:37 +0530

    commited

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import django_heroku
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 
 #}
 
-DATABASES = {
-    'default': dj_database_url.config()
-}
+#DATABASES = {
+ #   'default': dj_database_url.config()
+#}
 
 
 # Password validation
